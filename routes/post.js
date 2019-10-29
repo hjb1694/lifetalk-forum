f2b6672d5071f5885a25537c413344ee1a07675e
Hayden Bradfield <hbradfield20@gmail.com>
2019-10-29 11:03:52 -0400

    worked on new post page, added textarea and image input

--- routes/post.js
+++ routes/post.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
+const path = require('path');
+
 
 router.get('/', (req,res) => {
 
     res.render('newPost');
 
 });
-
-
 
 
 module.exports = router;
